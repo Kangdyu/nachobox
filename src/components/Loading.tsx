dc9106e9fadddd8fb508296d95379ebfb2e95393
Kangdyu <kangdyu99@gmail.com>
2020-11-08 20:58:33 +0900

    loading animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Container = styled.div`
   height: calc(100vh - ${(props) => props.theme.footerHeight});
@@ -9,8 +9,46 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const circleRotateAnimation = keyframes`
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const strokeOffsetAnimation = keyframes`
+  0% {
+    stroke-dashoffset: 0
+  }
+  75% {
+    stroke-dashoffset: -240
+  }
+  100% {
+    stroke-dashoffset: -251
+  }
+`;
+
+const LoadingCircle = styled.svg`
+  width: 90px;
+  height: 90px;
+  animation: ${circleRotateAnimation} 2s infinite;
+
+  circle {
+    stroke: ${({ theme }) => theme.colors.highlight};
+    stroke-width: 10;
+    fill: none;
+
+    animation: ${strokeOffsetAnimation} 1s infinite;
+  }
+`;
+
 function Loading() {
-  return <Container>Loading...</Container>;
+  return (
+    <Container>
+      <LoadingCircle strokeDasharray={251}>
+        <circle cx="50%" cy="50%" r="40"></circle>
+      </LoadingCircle>
+    </Container>
+  );
 }
 
 export default Loading;
