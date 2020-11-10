f82344aed418e221ed2499bd4db6c634a85024ef
Kangdyu <kangdyu99@gmail.com>
2020-11-10 12:17:01 +0900

    responsive: detail page

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -21,37 +21,68 @@ const MainGrid = styled.div`
   display: grid;
   grid-template-columns: 3fr 7fr;
   gap: 40px;
-  margin-bottom: 40px;
+  margin-bottom: 100px;
+
+  @media only screen and (max-width: 1200px) {
+    grid-template-columns: none;
+    grid-template-rows: auto auto;
+  }
+
+  @media only screen and (max-width: 768px) {
+  }
 `;
 
 const Poster = styled.img`
   width: 100%;
   height: auto;
+
+  @media only screen and (max-width: 1200px) {
+    width: 20%;
+    margin: 0 auto;
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 30%;
+    margin: 0 auto;
+  }
 `;
 
 const InfoContainer = styled.div`
   width: 100%;
+`;
+
+const MovieInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`;
 
-const MovieInfoContainer = styled.div``;
+  @media only screen and (max-width: 1200px) {
+    align-items: center;
+  }
+`;
 
 const Title = styled.h1`
   font-size: 2.5rem;
   font-weight: 700;
   margin-bottom: 15px;
+
+  @media only screen and (max-width: 1200px) {
+    font-size: 2rem;
+  }
 `;
 
 const DetailsContainer = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 20px;
+
+  @media only screen and (max-width: 1200px) {
+    font-size: 0.9rem;
+  }
 `;
 
 const DetailItem = styled.span`
-  display: inline-block;
+  display: block;
   color: #ccc;
 
   &:not(:last-child)::after {
@@ -81,12 +112,20 @@ const Rating = styled.span`
   font-size: 1.5rem;
   font-weight: 700;
   color: ${({ theme }) => theme.colors.highlight};
+
+  @media only screen and (max-width: 1200px) {
+    font-size: 1.2rem;
+  }
 `;
 
 const Overview = styled.div`
   font-size: 1.2rem;
   line-height: 2;
   margin-top: 30px;
+
+  @media only screen and (max-width: 1200px) {
+    font-size: 1rem;
+  }
 `;
 
 function Detail() {
